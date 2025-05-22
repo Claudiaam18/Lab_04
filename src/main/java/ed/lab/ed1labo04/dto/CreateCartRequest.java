ad570ec1a90004e6572a2a06f1513b64ba830d36
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-05-21 19:18:56 -0600

    Ejercicios

--- src/main/java/ed/lab/ed1labo04/dto/CreateCartRequest.java
+++ src/main/java/ed/lab/ed1labo04/dto/CreateCartRequest.java
@@ -1,10 +1,9 @@
 package ed.lab.ed1labo04.dto;
 
-import ed.lab.ed1labo04.dto.CartItemRequest;
-
 import java.util.List;
 
 public class CreateCartRequest {
+
     private List<CartItemRequest> cartItems;
 
     public List<CartItemRequest> getCartItems() {
